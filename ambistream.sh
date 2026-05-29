5c864db22f2759898ddf5ace464a1d278d503747
seprog <sebastianpoppenborg@gmail.com>
2026-05-30 01:43:25 +0200

    mostly reverted last commit but kept(/improved) basic structure reason: ambilight failed due to not yet supporting --ambilight-config argument

--- ambistream.sh
+++ ambistream.sh
@@ -1,1 +1,4 @@
-/opt/ambilight/venv/bin/python3 /opt/ambilight/main.py --stream rtmp://localhost:1935/$1/$2 --ambilight-config /opt/ambilight/config/$2
+#!/bin/sh
+
+
+/opt/ambilight/venv/bin/python3 /opt/ambilight/main.py --stream rtmp://localhost:1935/$1/$2  # TODO --ambilight-config /opt/ambilight/config/$2
