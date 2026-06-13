0f348e153c66df26f7d4cb31769e13170eede0ab
seprog <sebastianpoppenborg@gmail.com>
2026-06-13 02:06:28 +0200

    re-added config support (not-tested, should work now)

--- ambistream.sh
+++ ambistream.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 
-/opt/ambilight/venv/bin/python3 /opt/ambilight/main.py --stream rtmp://localhost:1935/$1/$2  # TODO --ambilight-config /opt/ambilight/config/$2
+/opt/ambilight/venv/bin/python3 /opt/ambilight/main.py --stream rtmp://localhost:1935/$1/$2 --config /opt/ambilight/config/$2.yaml
